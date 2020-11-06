9da90374005eeee27b41bff041641b7ea43f49c1
alertguo <1196614612@qq.com>
2020-11-06 15:30:37 +0800

    init

---
+++ src/vue-app.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>
+      看我如何在 Vue 里使用 echarts
+    </h1>
+    <vue-echarts :option="option"></vue-echarts>
+    <button @click="loadMore">加载更多</button>
+  </div>
+</template>
+
+<script lang="ts">
+import VueEcharts from './vue-echarts.vue';
+
+export default {
+  components: {VueEcharts},
+  data() {
+    return {
+      option: {
+        title: {
+          show: true,
+          text: '销量',
+          right: 20
+        },
+        legend: {
+          data: ['金额']
+        },
+        tooltip: {
+          show: true
+        },
+        xAxis: {
+          type: 'category',
+          data: ['2020-1-1','2020-1-2']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          name: '金额',
+          data: [1,2],
+          type: 'line'
+        }]
+      }
+    };
+  },
+  methods: {
+    loadMore() {
+      this.option = {
+        xAxis: {
+          data: ['2020-1-1', '2020-1-2', '2020-1-3']
+        },
+        series: [{
+          data: [1, 2, 3]
+        }]
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/vue-echarts.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="container">
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+
+export default {
+  props: ['option'],
+  mounted() {
+    console.log(this.$refs.container)
+    const width = document.documentElement.clientWidth
+    this.$refs.container.style.width = `${width - 20}px`
+    this.$refs.container.style.height = `${width * 1.2 - 20}px`
+    this.chart = echarts.init(this.$refs.container, 'dark')
+    this.chart.setOption(this.option)
+  },
+  watch: {
+    option() {
+      this.chart.setOption(this.option)
+    }
+  }
+}
+</script>
